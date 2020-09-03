e66192c729bc50c6ea7062890bf27c1576afed4d
Sunil Pandey <inkingmind@gmail.com>
2020-09-03 15:01:14 +0530

    non local code added

--- 09_global_variable.py
+++ 09_global_variable.py
@@ -1,5 +1,7 @@
 # Learning : if you are updationg global variable you have to use declare it as global in that scope
-
+# global : bring global x to local function when you want to update x which is in global scope, it will not affect local x if any declared
+# in outer func definition, 
+# non local : is generally used when we have nested functions, we bring outer func' x in the scope, not the global one
 gv = 12
 
 mutable_gv = {1,2,3}
@@ -19,5 +21,58 @@ def access_updated_global_variable() :
     print("updated gv -> ", gv) 
     print("updated mutable_gv -> ", mutable_gv) 
 
-update_global_variable()
-access_updated_global_variable()
+
+def demo_scope_test():
+    def do_local():
+        spam = "local spam"
+
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+
+# DEMOS
+#update_global_variable()
+#access_updated_global_variable()
+
+demo_scope_test()
+print("In global scope:", spam)
+
+def demo_global():
+    # print(x) # errorlocal variable 'x' referenced before assignment due to confusion
+    global x 
+    print(x)
+    x = "local"
+    print(x)
+
+
+def demo_non_local():
+    n = 50
+    def inner():
+        # nonlocal n  #only in nested function
+        global n # this will not change value n for global only not for the local n
+        n = 100
+    print(n)
+    inner()
+    print(n)
+    
+
+#demo_global()
+#print(x)
+
+#n = 20
+#demo_non_local()
+#print(n)
+
