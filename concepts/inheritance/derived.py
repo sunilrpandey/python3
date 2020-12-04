05f546bcbe79c68ae38e46eb8accadc250ededc4
Sunil Pandey <inkingmind@gmail.com>
2020-12-04 09:49:35 +0530

    super() is used to access base impl

--- concepts/inheritance/derived.py
+++ concepts/inheritance/derived.py
@@ -6,5 +6,6 @@ from base import Base
 
 class Derived(Base):
     def fun_impl(self):
-        # self.common_fun()
+        self.common_fun()
+        super().fun_impl()
         print("It's Derived")
